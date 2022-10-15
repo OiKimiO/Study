1f18e9a759cd2960f4d1cfa6b6c8319d5bf7e299
Eun Ho Kim <etkim02@naver.com>
2022-10-15 20:29:13 +0900

    스프링부트
    v5 진행

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v5/ControllerV3HandlerAdapter.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v5/ControllerV3HandlerAdapter.java
@@ -12,11 +12,23 @@ import hello.servlet.web.frontcontroller.v3.ControllerV3;
 
 public class ControllerV3HandlerAdapter implements MyHandlerAdapter{
 
+	
+	/** supports
+	 *  @param Object hadler
+	 *    - ControllerV3를 대처할 수 있는 컨트롤러를 뜻한다. 
+	 * */
 	@Override
 	public boolean supports(Object handler) {
+		// instanceof : handler가 ControllerV3 타입이거나 ControllerV3를 상속받는 클래스라면 
+		// true를 반환합니다.
 		return (handler instanceof ControllerV3);
 	}
 
+	/** handle
+	 *  @param HttpServletRequest request, HttpServletResponse response, Object handler
+	 *   - 
+	 * */
+	
 	@Override
 	public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws ServletException, IOException {
