1de24f17581ded5e0044bc2ce59371dae5614012
OiKimiO <53805469+OiKimiO@users.noreply.github.com>
2022-10-17 18:46:00 +0900

    스프링부트
    스프링부트
      - MVC 패턴 시작

--- coding_test/src/백준/DP/_2193_이친수_다시풀기.java
+++ coding_test/src/백준/DP/_2193_이친수_다시풀기.java
@@ -8,6 +8,16 @@ public class _2193_이친수_다시풀기 {
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = 91;
+		int[][] dp = new int[N][3];
+		dp[1][1] = 1;
+		dp[2][0] = 1;
+		dp[3][0] = 1;
+		dp[3][1] = 1;
+		dp[3][2] = 1;
+		for(int i = 4; i < N; i++) {
+			
+		}
 		
 	}
 	
