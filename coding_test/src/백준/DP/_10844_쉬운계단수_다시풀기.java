030b7492336fa5895abab7ba393429aebf682962
Eun Ho Kim <etkim02@naver.com>
2022-10-17 23:44:00 +0900

    스프링부트 & 알고리즘
    스프링부트
     - MVC 패턴
    알고리즘
     - DP 쉬운계단수

--- coding_test/src/백준/DP/_10844_쉬운계단수_다시풀기.java
+++ coding_test/src/백준/DP/_10844_쉬운계단수_다시풀기.java
@@ -7,7 +7,25 @@ public class _10844_쉬운계단수_다시풀기 {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] dp = new int[N+1][9];
 		
+		for(int i = 1; i <= 9; i++) {
+			dp[1][i] = 1;
+		}
+		
+		for(int i = 2; i <= N; i++) {
+			// dp[i][0] = 1;
+			for(int j = 0; j < i; j++) {
+				if(j-1 >= 0 && j+1 < 10){
+					dp[i][j] += dp[i-1][j-1] + dp[i-1][j+1];
+				}else if(j-1 < 0) {
+					dp[i][j] += dp[i-1][j+1];
+				}else if(j+1 > 9) {
+					dp[i][j] += dp[i-1][j-1];
+				}
+			}
+		}
 	}
 
 }
--- springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -5,23 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class LogTestController {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
-
+	
 	@RequestMapping("/log-test")
 	public String logTest() {
 		String name = "Spring";
 		
-		log.trace("trace log={}",name);
-		log.debug("debug log={}",name);
-		log.info("info log={}",name);
-		log.warn("warn log={}",name);
-		log.error("error log={}",name);
+		log.trace("trace log={}", name);
+		log.debug("trace debug={}", name);
+		log.info("trace info={}", name);
+		log.warn("trace warn={}", name);
+		log.error("trace error={}", name);
 		
-		// 로그를 사용하지 않아도 a+b 계산 로직이 먼저 실행됨, 이런 방식으로 사용하면 X
 		log.debug("String concat log = " + name);
 		
 		return "ok";
