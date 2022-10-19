f42a60e57d9ec72e8bab2baf5772d4626a1c0fb2
Eun Ho Kim <etkim02@naver.com>
2022-10-20 01:09:28 +0900

    스프링부트
    스프링부트
     - ResponseController

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -7,10 +7,15 @@ import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import hello.springmvc.basic.HelloData;
@@ -32,5 +37,64 @@ public class RequestBodyJsonController {
 		log.info("username={}, age={}", data.getUsername(), data.getAge());
 		
 		response.getWriter().write("ok");
+	}
+	
+	/**
+	 * @RequestBody
+	 * HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+	 *
+	 * @ResponseBody
+	 * - 모든 메서드에 @ResponseBody 적용
+	 * - 메시지 바디 정보 직접 반환(view 조회X)
+	 * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+	 */
+	@ResponseBody
+	@PostMapping("/request-body-json-v2")
+	public String requestBodyJsonV2(@RequestBody String messageBody) throws JsonMappingException, JsonProcessingException {
+		HelloData data = objectMapper.readValue(messageBody, HelloData.class);
+		log.info("username={}, age={}", data.getUsername(), data.getAge());
+		return "ok";
+	}
+	
+	/**
+	 * @RequestBody 생략 불가능(@ModelAttribute 가 적용되어 버림)
+	 * HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter (contenttype: application/json)
+	 *
+	 */
+	@ResponseBody
+	@PostMapping("request-body-json-v3")
+	public String requestBodyJsonV3(@RequestBody HelloData data) {
+		log.info("username={}, age={}", data.getUsername(), data.getAge());
+		return "ok";
+	}
+	
+	@ResponseBody
+	@PostMapping("/request-body-json-v4")
+	public String requestBodyJsonV4(HttpEntity<HelloData> httpEntity) {
+		HelloData data = httpEntity.getBody();
+		log.info("username={}, age={}", data.getUsername(), data.getAge());
+		return "ok";
+	}
+	
+	/**
+	 * @RequestBody 생략 불가능(@ModelAttribute 가 적용되어 버림)
+	 * HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter (contenttype: application/json)
+	 *
+	 * @ResponseBody 적용
+	 * - 메시지 바디 정보 직접 반환(view 조회X)
+	 * - HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter 적용
+	     (Accept: application/json)
+	     
+	     @RequestBody 요청
+		   - JSON 요청 > HTTP 메시지 컨버터 > 객체
+		 @ResponseBody 응답
+		   - 객체 > HTTP 메시지 컨버터 > JSON 응답
+	     
+	 */
+	@ResponseBody
+	@PostMapping("/request-body-json-v5")
+	public HelloData requestBodyJsonV5(@RequestBody HelloData data) {
+		log.info("username={}, age={}",data.getUsername(), data.getAge());
+		return data;
 	}
 }
