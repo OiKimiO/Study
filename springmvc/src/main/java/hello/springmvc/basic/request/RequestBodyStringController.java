9f80fbf0680227cc66ee5c9019ca0a93973ccb05
Eun Ho Kim <etkim02@naver.com>
2022-10-19 22:01:44 +0900

    스프링부트

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -7,9 +7,12 @@ import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -35,5 +38,37 @@ public class RequestBodyStringController {
 		String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 		log.info("messageBody={}",messageBody);
 		responseWriter.write("ok");
+	}
+	
+	/**
+	 * HttpEntity: HTTP header, body 정보를 편리하게 조회
+	 * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
+	 * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+	 *
+	 * 응답에서도 HttpEntity 사용 가능
+	 * - 메시지 바디 정보 직접 반환(view 조회X)
+	 * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+	 */
+	@PostMapping("/request-body-string-v3")
+	public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity){
+		String messageBody = httpEntity.getBody();
+		log.info("message={}",messageBody);
+		return new HttpEntity<>("ok");
+	}
+	
+	/**
+	 * @RequestBody
+	 * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
+	 * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+	 *
+	 * @ResponseBody
+	 * - 메시지 바디 정보 직접 반환(view 조회X)
+	 * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+	 */
+	@ResponseBody
+	@PostMapping("/request-body-string-v4")
+	public String requestBodyStringV4(@RequestBody String messageBody) {
+		log.info("message={}",messageBody);
+		return "ok";
 	}
 }
