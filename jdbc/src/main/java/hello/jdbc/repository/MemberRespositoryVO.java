f731985fbe31df61cd949ab62f0278dbc48dc27f
Eun Ho Kim <etkim02@naver.com>
2023-03-09 22:03:11 +0900

    스프링 DB1
    h2 데이터베이스
    JDBC, ORM 개념 및 기술
    트러블 슈팅

---
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRespositoryVO.java
@@ -0,0 +1,101 @@
+package hello.jdbc.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.NoSuchElementException;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MemberRespositoryVO {
+
+	public Member save(Member member) throws SQLException{
+		String sql = "insert into member(member_id, money) values(?,?)";
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con   = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1,member.getMemberId());
+			pstmt.setInt(2,member.getMoney());
+			pstmt.executeUpdate();
+			return member;
+		} catch (SQLException e) {
+			log.error("db error" , e);
+			throw e;
+		}finally {
+			close(con, pstmt, null);
+		}
+	}
+	
+	public Member findById(String memberId) throws SQLException {
+		String sql = "select * from member where member_id = ?";
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				Member member = new Member();
+				member.setMemberId(rs.getString("member_id"));
+				member.setMoney(rs.getInt("money"));
+				return member;
+			}else {
+				throw new NoSuchElementException("member not found Exception = "+memberId);
+			}
+		}catch (SQLException e) {
+			log.error("db error" , e);
+			throw e;
+		}finally {
+			close(con, pstmt, rs);
+		}
+				
+	}
+	
+	// 리소스를 반환할 때는 역순으로 종료
+	private void close(Connection con, 
+					   Statement pstmt, 
+					   ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				log.info("error", e);
+			}
+		}
+		
+		if(pstmt != null) {
+			try {
+				pstmt.close(); // SQLException
+			} catch (SQLException e) {
+				log.info("error", e);
+			}
+		}
+		
+		if(con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				log.info("error", e);
+			}
+		}
+		
+	}
+	
+	public Connection getConnection() {
+		return DBConnectionUtil.getConnection();
+	}
+}
